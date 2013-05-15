1b46f56e25fc0422bf8ed32b9ee7a53c2ee3bc56
damanfb <daman.bareiss@utah.edu>
2013-05-15 15:16:55 -0600

    Added new ROS files

---
+++ build/CMakeFiles/drone_control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/drone_control.dir/src/ardrone_controller.o"
+  "../bin/drone_control.pdb"
+  "../bin/drone_control"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/drone_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
